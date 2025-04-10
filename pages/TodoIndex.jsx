bd821a6f605a32d8e3052a8ef5139cf5fdde2cd3
WhiteBeltFullStack <zangetsu923@gmail.com>
2025-04-10 16:17:27 +0300

    Initial Repo and make first adjustment to crud function using redux

---
+++ pages/TodoIndex.jsx
@@ -0,0 +1,70 @@
+import { TodoFilter } from '../cmps/TodoFilter.jsx'
+import { TodoList } from '../cmps/TodoList.jsx'
+import { DataTable } from '../cmps/data-table/DataTable.jsx'
+import { todoService } from '../services/todo.service.js'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service.js'
+import { loadTodos, removeTodo, saveTodo } from '../store/actions/todo.actions.js'
+
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
+const { useSelector, useDispatch } = ReactRedux
+
+export function TodoIndex() {
+  //   const [todos, setTodos] = useState(null) FROM THIS
+  const todos = useSelector(storeState => storeState.todos) // TO THIS
+  const dispatch = useDispatch()
+
+  // Special hook for accessing search-params:
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const defaultFilter = todoService.getFilterFromSearchParams(searchParams)
+
+  const [filterBy, setFilterBy] = useState(defaultFilter)
+
+  useEffect(() => {
+    setSearchParams(filterBy)
+    loadTodos(filterBy).catch(() => {
+      showErrorMsg('Cannnot load Todos')
+    })
+  }, [filterBy])
+
+  function onRemoveTodo(todoId) {
+    const isSureDelete = confirm('Are you sure')
+    if (!isSureDelete) return
+
+    removeTodo(todoId).catch(() => {
+      showErrorMsg('cannot remove todo')
+    })
+  }
+
+  function onToggleTodo(todo) {
+    const todoToSave = { ...todo, isDone: !todo.isDone }
+    saveTodo(todoToSave)
+      .then(savedTodo => {
+        showSuccessMsg(`Todo is ${savedTodo.isDone ? 'done' : 'back on your list'}`)
+      })
+      .catch(err => {
+        console.log('err:', err)
+        showErrorMsg('Cannot toggle todo ' + todoToSave._id)
+      })
+  }
+
+  if (!todos) return <div>Loading...</div>
+  return (
+    <section className="todo-index">
+      <TodoFilter filterBy={filterBy} onSetFilterBy={setFilterBy} />
+      <div>
+        <Link to="/todo/edit" className="btn">
+          Add Todo
+        </Link>
+      </div>
+      <h2>Todos List</h2>
+      <TodoList todos={todos} onRemoveTodo={onRemoveTodo} onToggleTodo={onToggleTodo} />
+      <hr />
+      <h2>Todos Table</h2>
+      <div style={{ width: '60%', margin: 'auto' }}>
+        <DataTable todos={todos} onRemoveTodo={onRemoveTodo} />
+      </div>
+    </section>
+  )
+}
---
+++ store/store.js
@@ -0,0 +1,68 @@
+import { userService } from '../services/user.service.js'
+const { createStore, compose } = Redux
+//TODOS
+export const SET_TODOS = 'SET_TODOS'
+export const REMOVE_TODO = 'REMOVE_TODO'
+export const ADD_TODO = 'ADD_TODO'
+export const UPDATE_TODO = 'UPDATE_TODO'
+//USER
+export const SET_USER = 'SET_USER'
+export const SET_USER_BALANCE = 'SET_USER_BALANCE'
+
+const initialState = {
+  loggedInUser: userService.getLoggedinUser(),
+  todos: [],
+  isLoading: false,
+}
+
+function appReducer(state = initialState, cmd = {}) {
+  switch (cmd.type) {
+    //TODOS
+    case SET_TODOS:
+      return {
+        ...state,
+        todos: cmd.todos,
+      }
+
+    case REMOVE_TODO:
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo._id !== cmd.todoId),
+      }
+
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [...state.todos, cmd.todo],
+      }
+
+    case UPDATE_TODO:
+      return {
+        ...state,
+        todos: state.todos.map(todo =>
+          todo._id === cmd.todo._id ? cmd.todo : todo
+        ),
+      }
+    //USER
+    case SET_USER:
+      return {
+        ...state,
+        loggedInUser: cmd.loggedInUser,
+      }
+
+    case SET_USER_BALANCE:
+      const loggedInUser = { ...state.loggedInUser, balance: cmd.balance }
+      return {
+        ...state,
+        loggedInUser,
+      }
+  }
+
+  return state
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+export const store = createStore(appReducer, composeEnhancers())
+
+// * For Debugging
+window.gStore = store
